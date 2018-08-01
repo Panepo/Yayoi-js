32c7478104c355c563bdcc774a87d7c77edcc1fd
Panepo <whisper.ko@gmail.com>
2018-08-01 10:01:40 +0800

    modify function
    modify handleupload function to get upload image's size

--- src/components/ModelSRCNN.jsx
+++ src/components/ModelSRCNN.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 import * as tf from '@tensorflow/tfjs'
 import './ModelSRCNN.css'
@@ -10,7 +11,11 @@ export default class ModelSRCNN extends Component {
       file: null,
       text: '',
       contentDisplay: false,
-      isLoading: true
+      isLoading: false,
+      imageWidth: 100,
+      imageHeight: 100,
+      canvasWidth: 200,
+      canvasHeight: 200
     }
     this.handleUpload = this.handleUpload.bind(this)
   }
@@ -20,34 +25,43 @@ export default class ModelSRCNN extends Component {
     setTimeout(() => this.setState({ isLoading: false }), 5000)
   }
 
-  componentDidUpdate() {
-    const canvas = this.refs.outputCanvas
-    const ctx = canvas.getContext('2d')
-    const image = this.refs.inputImage
-
-    image.onload = () => {
-      ctx.drawImage(image, 0, 0)
-    }
-  }
-
   handleUpload(event) {
-    const { uploaderID } = this.props
-    let textId = uploaderID + '_imageUploader_text_div'
+    const textDiv = this.refs.textDiv
 
     if (event.target.files[0] != null) {
-      document.getElementById(textId).classList.add('is-focused')
+      textDiv.classList.add('is-focused')
       this.setState({
         file: URL.createObjectURL(event.target.files[0]),
         text: event.target.files[0].name,
         contentDisplay: true
       })
+      this.canvasDraw()
     } else {
-      document.getElementById(textId).classList.remove('is-focused')
+      textDiv.classList.remove('is-focused')
       this.setState({
         file: null,
         text: '',
         contentDisplay: false
       })
+    }
+  }
+
+  canvasDraw = () => {
+    const canvasi = this.refs.inputCanvas
+    const ctxi = canvasi.getContext('2d')
+    const image = ReactDOM.findDOMNode(this.refs.inputImage)
+
+    image.onload = () => {
+      this.setState({
+        imageWidth: image.naturalWidth,
+        imageHeight: image.naturalHeight,
+        canvasWidth: image.naturalWidth * 2,
+        canvasHeight: image.naturalHeight * 2
+      })
+      ctxi.clearRect(0, 0, this.state.imageWidth, this.state.imageHeight)
+      ctxi.drawImage(image, 0, 0)
+
+      this.modelPredict(canvasi)
     }
   }
 
@@ -57,35 +71,30 @@ export default class ModelSRCNN extends Component {
     this.model.predict(tf.zeros([1, 32, 32, 1])).dispose()
   }
 
-  modelPredict = async () => {
-    await tf.tidy(() => {
-      const image = this.refs.inputImage
-      const img = tf.fromPixels(image).toFloat()
-      const offset = tf.scalar(255)
+  modelPredict = input => {
+    const canvaso = this.refs.outputCanvas
+    const ctxo = canvaso.getContext('2d')
+    ctxo.clearRect(0, 0, this.state.canvasWidth, this.state.canvasHeight)
 
-      const normalized = img.div(offset)
-      const batched = normalized.reshape([1, 32, 32, 1])
-      const result = this.model.predict(batched)
-
-      console.log(result)
-    })
+    const imageData = tf
+      .fromPixels(input)
+      .toFloat()
+      .div(tf.scalar(255))
+    const imageScale = tf.image.resizeBilinear(
+      imageData,
+      [this.state.canvasWidth, this.state.canvasHeight],
+      false
+    )
+    tf.toPixels(imageScale, canvaso)
   }
 
   render() {
-    const {
-      uploaderID,
-      uploaderClass,
-      buttonText,
-      imageWidth,
-      canvasWidth,
-      canvasHeight
-    } = this.props
-    let textId = uploaderID + '_imageUploader_text_div'
+    const { uploaderClass, buttonText } = this.props
 
     let canvasClass = ''
     if (this.state.contentDisplay) {
       canvasClass =
-        'layout-content mdl-color--white mdl-shadow--4dp mdl-color-text--grey-800 mdl-cell mdl-cell--7-col'
+        'layout-content mdl-color--white mdl-shadow--4dp mdl-color-text--grey-800 mdl-cell mdl-cell--6-col'
     } else {
       canvasClass = 'canvas-none'
     }
@@ -104,7 +113,7 @@ export default class ModelSRCNN extends Component {
       return (
         <div className="mdl-grid">
           <div className="mdl-cell mdl-cell--1-col mdl-cell--hide-tablet mdl-cell--hide-phone" />
-          <div className="layout-content mdl-color--white mdl-shadow--4dp mdl-color-text--grey-800 mdl-cell mdl-cell--3-col">
+          <div className="layout-content mdl-color--white mdl-shadow--4dp mdl-color-text--grey-800 mdl-cell mdl-cell--4-col">
             <div className="imageUploader">
               <label className={uploaderClass}>
                 {buttonText}
@@ -115,7 +124,7 @@ export default class ModelSRCNN extends Component {
                 />
               </label>
             </div>
-            <div id={textId} className="mdl-textfield mdl-js-textfield">
+            <div ref="textDiv" className="mdl-textfield mdl-js-textfield">
               <input
                 className="imageUploader_text mdl-textfield__input"
                 type="text"
@@ -129,22 +138,30 @@ export default class ModelSRCNN extends Component {
                 htmlFor="imageUploader_text"
               />
             </div>
-            <div>
+            <div className="canvas-none">
               <p className="imageUploader_image">
                 <img
                   ref="inputImage"
                   src={this.state.file}
-                  width={imageWidth}
+                  width="100%"
+                  height="100%"
                   alt={this.state.text}
                 />
               </p>
+            </div>
+            <div>
+              <canvas
+                ref="inputCanvas"
+                width={this.state.imageWidth}
+                height={this.state.imageHeight}
+              />
             </div>
           </div>
           <div className={canvasClass}>
             <canvas
               ref="outputCanvas"
-              width={canvasWidth}
-              height={canvasHeight}
+              width={this.state.canvasWidth}
+              height={this.state.canvasHeight}
             />
           </div>
         </div>
@@ -154,22 +171,14 @@ export default class ModelSRCNN extends Component {
 }
 
 ModelSRCNN.propTypes = {
-  uploaderID: PropTypes.string,
   uploaderClass: PropTypes.string,
   buttonText: PropTypes.string,
-  imageWidth: PropTypes.number,
-  canvasWidth: PropTypes.number,
-  canvasHeight: PropTypes.number,
   modelPath: PropTypes.string
 }
 
 ModelSRCNN.defaultProps = {
-  uploaderID: 'ImageUploader',
   uploaderClass:
     'imageUploader_Button mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--primary',
   buttonText: 'Select Image',
-  imageWidth: 300,
-  canvasWidth: 600,
-  canvasHeight: 600,
   modelPath: './model/model.json'
 }
